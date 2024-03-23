60cfb7af903e11fd66b6cfca7059c5ca7828f007
Nikita Filippov <nikita.filippov.aleks@mail.ru>
2024-03-23 13:18:44 +0300

    Basics folder

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:vanillacontacts_course/inherited_and_change_notifier/app.dart';
+import 'package:vanillacontacts_course/basics/inherited_and_change_notifier/app.dart';
 
 void main() {
   runApp(const MyApp());
